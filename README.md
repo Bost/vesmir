e3c74c563f1c15521f0def2cff640b2452f6118b
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-12-23 12:51:32 +0100

    amazon s3

--- README.md
+++ README.md
@@ -1,8 +1,63 @@
-## Deploy to Heroku
-```bash
-<!-- heroku buildpacks:set https://github.com/lexi-lambda/heroku-buildpack-racket -->
-heroku config:set RACKET_VERSION=8.1
-<!-- heroku config:set PAPERTRAIL_API_TOKEN=<papertrail-token> -->
-<!-- heroku config:set BOT_TOKEN=<bot-token> -->
-git push heroku master
+## Install Heroku
+```fish
+# on Ubuntu:
+# The `sudo snap install heroku --classic` doesn't work on Ubuntu
+# See https://github.com/heroku/cli/issues/822
+curl https://cli-assets.heroku.com/install.sh | sh
+
+# on Guix:
+npm install -g heroku
+```
+
+## PostgreSQL
+```fish
+## on Ubuntu:
+sudo apt install --yes postgresql postgresql-contrib
+# TODO see the the proposition given by the installation process:
+# You can now start the database server using:
+#     pg_ctlcluster 13 main start
+sudo systemctl status postgresql.service
+sudo systemctl stop postgresql.service
+mkdir -p ./var/log/
+sudo chmod --recursive u=rwx,g=rwx,o=rwx ./var/
+sudo --shell --user=postgres
+# when using fish shell:
+set --export PATH /usr/lib/postgresql/*/bin $PATH
+initdb ./var/pg # dropdb postgres && rm -rf ./var/pg
+pg_ctl -D ./var/pg -l ./var/log/postgres.log start
+# see also: postgres -D ./var/pg &
+
+## on GuixOS:
+pg_ctl -D ./var/pg -l ./var/log/postgres.log start
+```
+
+
+## Run on / Deploy to Heroku
+```fish
+set APP <...>
+heroku buildpacks:set https://github.com/lexi-lambda/heroku-buildpack-racket --app $APP
+heroku config:set RACKET_VERSION=8.3 --app $APP
+# heroku config:set PAPERTRAIL_API_TOKEN=<papertrail-token>
+# heroku config:set BOT_TOKEN=<bot-token>
+git remote add heroku https://git.heroku.com/$APP.git
+git push heroku main
+```
+
+## Test Heroku
+```fish
+curl --request  GET https://$APP.herokuapp.com/values
+curl --request POST -H 'Content-Type: application/json' -d '{"x":"1", "y":"2"}' \
+     https://$APP.herokuapp.com/values
+
+```
+## Run locally
+```fish
+racket --require server.rkt
+```
+
+## Test locally
+```fish
+curl --request  GET http://localhost:8080/values
+curl --request POST -H 'Content-Type: application/json' -d '{"x":"1", "y":"2"}' \
+     http://localhost:8080/values
 ```
--- server.rkt
+++ server.rkt
@@ -1,53 +1,75 @@
 #lang racket
 
-;; See Alexis King's & the forks
-;; https://github.com/lexi-lambda/racket-sample-heroku-app
+;; See https://defn.io/2020/02/12/racket-web-server-guide/
 
-;; Luis Quintanilla: Building Web Services with Racket
-;; https://youtu.be/ZvwE6_MMJjM
-;; https://github.com/lqdev/Presentations/tree/master/Reactor022021-Racket-Web-Services
-(require json)
-(require web-server/servlet)
-(require web-server/servlet-env)
+(require (prefix-in fotos: "files.rkt"))
 
-;;; Convert raw request body to jsexrp
-(define (parse-json-body req)
-  (bytes->jsexpr (request-post-data/raw req)))
+(require net/url
+         web-server/dispatch
+         web-server/servlet
+         web-server/servlet-env
+         (prefix-in files: web-server/dispatchers/dispatch-files)
+         (prefix-in filter: web-server/dispatchers/dispatch-filter)
+         (prefix-in sequencer: web-server/dispatchers/dispatch-sequencer)
+         web-server/dispatchers/filesystem-map
+         web-server/http
+         web-server/servlet-dispatch
+         web-server/web-server)
 
-;;; Function to get value from hash given a key
-(define (get-hash-value h v)
-  (hash-ref h v))
+
+(define (response/template . content)
+  (response/xexpr
+   `(html
+     (head
+      (link ([href "/static/screen.css"] [rel "stylesheet"])))
+     (body
+      ,@content))))
+
+(define (homepage req)
+  (response/template
+   #;'(h1 "Home")
+   (cons 'div
+         (fotos:tags))
+   #;'(div
+     #;(div "a div in a div")
+     #;(a ([target "_blank"] [href "static/pics/img1.jpeg"])
+        (img ([src "static/pics/img1.jpeg"])))
+     #;(a ([target "_blank"] [href "static/pics/img2.jpeg"])
+        (img ([src "static/pics/img2.jpeg"])))
+
+     #;(a ([target "_blank"] [href "https://vesmir.s3.eu-central-1.amazonaws.com/2021-12-01_Martin/20211222_141930.jpg"])
+        (img ([src "https://vesmir.s3.eu-central-1.amazonaws.com/2021-12-01_Martin/20211222_141930.jpg"])))
+     )))
+
+(define (blog req)
+  (response/template '(h1 "Blog")))
+
+(define (not-found req)
+  (response/template '(h1 "Not Found")))
+
+(define-values (app reverse-uri)
+  (dispatch-rules
+   [("") homepage]
+   [("blog") blog]))
+
+(define url->path/static (make-url->path "static"))
+
+(define static-dispatcher
+  (files:make #:url->path (lambda (u)
+                            (url->path/static
+                             (struct-copy url u [path (cdr (url-path u))])))))
+
+
+(define-values (dispatch req)
+  (dispatch-rules
+   [("") #:method "get" homepage]
+   [("static") #:method "get" static-dispatcher]
+   [else (error "Route does not exist")]))
 
 (define port (if (getenv "PORT")
                  (string->number (getenv "PORT"))
-                 8080))
+                 8000))
 
-;;; Define handlers
-(define (get-values req)
-  (response/jsexpr
-   (hasheq
-    'values '(1 2 3)
-    'ITA (hasheq
-          'dates '(4 5 6)))))
-
-(define (post-values req)
-  (define get-property
-    (curry get-hash-value (parse-json-body req)))
-
-  (define x (string->number (get-property 'x)))
-  (define y (string->number (get-property 'y)))
-
-  (response/jsexpr
-   (hasheq 'sum (+ x y))))
-
-;;; Define router
-(define-values (dispatch req)
-  (dispatch-rules
-   [("values") #:method "get" get-values]
-   [("values") #:method "post" post-values]
-   [else (error "Route does not exist")]))
-
-;; Define and start server
 (serve/servlet
  (lambda (req) (dispatch req))
  ;; #:launch-browser? #f
@@ -62,3 +84,25 @@
  ;; browser and don't print the DrRacket banner:
  #:command-line? #t
  )
+
+#;(define stop
+  (serve
+   #:dispatch (sequencer:make
+               (filter:make #rx"^/static/" static-dispatcher)
+               (dispatch/servlet app)
+               (dispatch/servlet not-found))
+   #:listen-ip #f ;; "127.0.0.1"
+   #:port port
+   #:command-line? #t))
+
+#;(with-handlers ([exn:break? (lambda (e)
+                              (stop))])
+  (sync/enable-break never-evt))
+
+#;(serve/servlet
+ static-dispatcher
+ #:listen-ip "127.0.0.1"
+ #:port 8000
+ #:command-line? #t
+ #:servlet-path ""
+ #:servlet-regexp #rx"")
