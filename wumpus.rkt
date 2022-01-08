b071fcebeda10c7f9ce069bf8ec57ca2f79b32eb
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2022-01-08 14:57:58 +0100

    wip: games

---
+++ wumpus.rkt
@@ -0,0 +1,8 @@
+#lang racketscript/base
+
+(require racketscript/htdp/universe
+         racketscript/htdp/image
+         racketscript/interop
+         racketscript/browser)
+
+(displayln "Hello World!")
