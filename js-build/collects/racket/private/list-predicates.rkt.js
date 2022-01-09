950c463beb87df14af848fa753c24da0be4d4bee
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2022-01-09 16:38:34 +0100

    info.rkt: add racketscript to build-deps; remove js-build/

--- js-build/collects/racket/private/list-predicates.rkt.js
+++
@@ -1,1 +0,0 @@
-import * as $rjs_core from '../../../runtime/core.js';import * as M0 from "../../../runtime/kernel.rkt.js";var empty_p = function(l2249) {return M0.null_p(l2249);};var cons_p = function(l2250) {return M0.pair_p(l2250);};var __rjs_quoted__ = {};export { __rjs_quoted__,cons_p,empty_p };
--- js-build/runtime/runtime.rkt.js
+++
@@ -1,1 +0,0 @@
-import * as $rjs_core from './core.js';import * as M1 from "./lib.rkt.js";var values = function() {var vals133 = M1.Core.argumentsToArray(arguments);if ((vals133.length===1)!==false) {var if_res27 = vals133[0];} else {var if_res27 = M1.Values.make(vals133);}return if_res27;};var __rjs_quoted__ = {};export { __rjs_quoted__,values };
